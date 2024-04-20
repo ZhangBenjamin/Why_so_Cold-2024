6c647bf8f131e8b7a6cb33cdc8f4ea24b2381bb3
Zhijie Kuang <91805013+ZhijieK@users.noreply.github.com>
2024-04-20 14:12:09 -0400

    Add files via upload

---
+++ FaceDetection.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+# Read the input image
+img = cv2.imread('test.jpg')
+
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+# Draw rectangle around the faces and detect smile
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+
+    for (sx, sy, sw, sh) in smiles:
+        cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
