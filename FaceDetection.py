a1ff0ae2d904a7acbea8b8bda8bd91254d8b9759
Zhijie Kuang <91805013+ZhijieK@users.noreply.github.com>
2024-04-20 14:13:01 -0400

    Add files via upload

--- FaceDetection.py
+++ FaceDetection.py
@@ -4,25 +4,36 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
-# Read the input image
-img = cv2.imread('test.jpg')
+# To capture video from webcam
+cap = cv2.VideoCapture(0)
 
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    # Read the frame
+    _, img = cap.read()
 
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Draw rectangle around the faces and detect smile
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    for (sx, sy, sw, sh) in smiles:
-        cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
 
-# Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+# Release the VideoCapture object
+cap.release()
