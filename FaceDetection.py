b98addb1fc1067e265f8ee650bfba752dba0cf24
Zhijie Kuang <91805013+ZhijieK@users.noreply.github.com>
2024-04-20 15:00:06 -0400

    Add files via upload

--- FaceDetection.py
+++ FaceDetection.py
@@ -22,14 +22,14 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
 
     # For each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        smiles = smile_cascade.detectMultiScale(roi_gray, 1.9, 22)
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.9, minNeighbors=22)
 
         for (sx, sy, sw, sh) in smiles:
             cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
