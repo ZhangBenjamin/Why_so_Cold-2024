e62ece88a43d26466b3e08d33cca9b4ba5408a44
Zhijie Kuang <91805013+ZhijieK@users.noreply.github.com>
2024-04-20 14:53:47 -0400

    Add files via upload

--- FaceDetection.py
+++ FaceDetection.py
@@ -29,17 +29,15 @@ while True:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 25)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.9, 22)
 
-        # Only consider it a smile if there are more than 5 detected regions
-        if len(smiles) > 5:
-            for (sx, sy, sw, sh) in smiles:
-                cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
-                if time.time() - last_pic_time >= 1:  # At least 1 second since last picture
-                    smile_frame_counter += 1
-                    cv2.imwrite(f'smile_frame_{smile_frame_counter}.jpg', img)
-                    print(f'Smile detected! Picture taken and saved as smile_frame_{smile_frame_counter}.jpg')
-                    last_pic_time = time.time()
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+            if time.time() - last_pic_time >= 1:  # At least 1 second since last picture
+                smile_frame_counter += 1
+                cv2.imwrite(f'smile_frame_{smile_frame_counter}.jpg', img)
+                print(f'Smile detected! Picture taken and saved as smile_frame_{smile_frame_counter}.jpg')
+                last_pic_time = time.time()
 
     # Display
     cv2.imshow('img', img)
